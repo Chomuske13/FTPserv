5ec08f0b248bb1d0f9d624015c91ff4b649b2dfa
Honey_boy <grigorovich2005@list.ru>
2025-03-30 21:13:02 +0300

    Update ftp_server.h

--- ftp_server.h
+++ ftp_server.h
@@ -28,28 +28,22 @@ typedef struct {
     int addr_len;
 } client_t;
 
-// Основные функции сервера
 void start_server();
 void stop_server();
 void show_server_status();
 
-// Функции для работы с клиентами
 void *handle_client(void *arg);
 void send_response(int sock, const char *response);
 
-// Функции для работы с файлами
 int send_file(int sock, const char *filename);
 int recv_file(int sock, const char *filename);
 void list_files(int sock);
 
-// Функции для работы с архивами
 void compress_to_tar_gz(const char *dirpath, const char *output_file);
 void extract_tar_gz(const char *filename, const char *output_dir);
 
-// Обработка команд
 void process_command(int sock, const char *command, const char *arg1, const char *arg2);
 
-// Интерфейс
 void init_interface();
 void run_interface();
 void show_help();
